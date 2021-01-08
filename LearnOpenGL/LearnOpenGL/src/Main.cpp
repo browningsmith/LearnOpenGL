a45f48875be6c1ffd1fe6be9f0c6fd360a56996d
Browning Smith <browningsmith@email.arizona.edu>
2021-01-08 02:33:05 -0700

    Converted Main.cpp to match Unreal Coding Standard as much as possible

--- LearnOpenGL/LearnOpenGL/src/Main.cpp
+++ LearnOpenGL/LearnOpenGL/src/Main.cpp
@@ -5,12 +5,12 @@
 
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
+void FramebufferSizeCallback(GLFWwindow* Window, int Width, int Height);
+void ProcessInput(GLFWwindow* Window);
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const unsigned int ScreenWidth = 800;
+const unsigned int ScreenHeight = 600;
 
 int main()
 {
@@ -27,15 +27,15 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
+    GLFWwindow* Window = glfwCreateWindow(ScreenWidth, ScreenHeight, "LearnOpenGL", NULL, NULL);
+    if (Window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwMakeContextCurrent(Window);
+    glfwSetFramebufferSizeCallback(Window, FramebufferSizeCallback);
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
@@ -47,11 +47,11 @@ int main()
 
     // build and compile our shader program
     // ------------------------------------
-    FShaderProgram ourShader("resources/shaders/shader.vs", "resources/shaders/shader.fs"); // you can name your shader files however you like
+    FShaderProgram OurShader("resources/shaders/shader.vs", "resources/shaders/shader.fs"); // you can name your shader files however you like
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
-    float vertices[] = {
+    float Vertices[] = {
         // positions         // colors
          0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
         -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
@@ -59,21 +59,21 @@ int main()
     };
 
     //Texture coordinates
-    float texCoordinates[] = {
+    float TexCoordinates[] = {
 
         0.0f, 0.0f,
         1.0f, 0.0f,
         0.5f, 1.0f
     };
 
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
+    unsigned int VertexBufferObjectID, VertexArrayObjectID;
+    glGenVertexArrays(1, &VertexArrayObjectID);
+    glGenBuffers(1, &VertexBufferObjectID);
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
+    glBindVertexArray(VertexArrayObjectID);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VertexBufferObjectID);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
@@ -89,11 +89,11 @@ int main()
 
     // render loop
     // -----------
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(Window))
     {
         // input
         // -----
-        processInput(window);
+        ProcessInput(Window);
 
         // render
         // ------
@@ -101,20 +101,20 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         // render the triangle
-        ourShader.Use();
-        glBindVertexArray(VAO);
+        OurShader.Use();
+        glBindVertexArray(VertexArrayObjectID);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(Window);
         glfwPollEvents();
     }
 
     // optional: de-allocate all resources once they've outlived their purpose:
     // ------------------------------------------------------------------------
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &VertexArrayObjectID);
+    glDeleteBuffers(1, &VertexBufferObjectID);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
@@ -124,17 +124,17 @@ int main()
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow* window)
+void ProcessInput(GLFWwindow* Window)
 {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(Window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(Window, true);
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void FramebufferSizeCallback(GLFWwindow* Window, int Width, int Height)
 {
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
+    glViewport(0, 0, Width, Height);
 }
